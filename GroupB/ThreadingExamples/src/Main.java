5d763f90f08d6ce9db7e59c06173c93a13cf7870
mmurray <michael.murray@gmit.ie>
2020-10-15 16:56:13 +0100

    15/10/2020 ... Threading project

---
+++ GroupB/ThreadingExamples/src/Main.java
@@ -0,0 +1,81 @@
+public class Main extends Thread {
+
+    public static void main(String[] args) {
+        Thread t0 = new MyThread();
+        t0.start();
+        Thread t1 = new MyThread();
+        t1.start();
+        Thread t2 = new Main();
+        t2.start();
+        Thread t3 = new Thread() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 10; i++) {
+                    System.out.println("Hello from Thread " + Thread.currentThread().getName());
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        };
+        t3.start();
+        Thread t4 = new Thread(new MyRunnable());
+        t4.start();
+        Thread t5 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i <10;i++) {
+                    System.out.println("Hello from Thread " + Thread.currentThread().getName());
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+        t5.start();
+        new Thread() {
+            @Override
+            public void run() {
+                for (int i = 0; i <10;i++) {
+                    System.out.println("Hello from Thread " + Thread.currentThread().getName());
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }.start();
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i <10;i++) {
+                    System.out.println("Hello from Thread " + Thread.currentThread().getName());
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }).start();
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i <10;i++) {
+            System.out.println("Hello from Thread " + Thread.currentThread().getName());
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+}
